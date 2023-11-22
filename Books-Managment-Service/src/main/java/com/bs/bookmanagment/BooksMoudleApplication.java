e7e9e25d07aea65b543e046b6e3d4ad4501f5504
AkshayVarsha66 <151723308+AkshayVarsha66@users.noreply.github.com>
2023-11-22 14:10:10 +0530

    Add files via upload

---
+++ Books-Managment-Service/src/main/java/com/bs/bookmanagment/BooksMoudleApplication.java
@@ -0,0 +1,13 @@
+package com.bs.bookmanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksMoudleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksMoudleApplication.class, args);
+	}
+
+}
---
+++ Books-Managment-Service/src/test/java/com/bs/bookmanagment/BooksMoudleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bs.bookmanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksMoudleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
