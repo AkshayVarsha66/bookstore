77b76c0b8243857a0e8845e830d1a3df790c56e0
AkshayVarsha66 <151723308+AkshayVarsha66@users.noreply.github.com>
2023-11-22 15:45:16 +0530

    Add files via upload

---
+++ Student/src/main/java/com/student/service/ServiceImp.java
@@ -0,0 +1,28 @@
+package com.student.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.RequestEntity;
+
+import com.student.entity.Student;
+import com.student.repository.StudentRepository;
+
+public class ServiceImp implements StudentService{
+
+	@Autowired
+	private StudentRepository studentRepository;
+
+	@Override
+	public Student addstudent(Student student) {
+		studentRepository.save(student);
+		return student;
+	}
+
+	@Override
+	public List<Student> allstudent() {
+		List<Student> allstudents = studentRepository.findAll();
+		return allstudents;
+	}
+
+}
