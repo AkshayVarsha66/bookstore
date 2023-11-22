e336050a41755444721b977f72bb81cc413ddc93
axo020301 <axo020301@darden.com>
2023-11-22 14:27:25 +0530

    Initial Commit

---
+++ Admin-Service/src/main/java/com/bs/practice/Sahana.java
@@ -0,0 +1,30 @@
+package com.bs.practice;
+
+import java.util.Scanner;
+
+public class Sahana {
+
+	static void method1(int k) {
+	}
+
+	public static void main(String[] args) {
+
+		byte a = 0;
+		int c = 90;
+		float io=899.12345f;
+		double p = 2147483648.165678956789;
+		String l = "sahana";
+		System.out.println(p);
+
+		method1(c);
+	}
+
+}
+	
+/*
+ * String[] args = { "int c = 90", String l = "ko";
+ * System.out.println("output 1--"+c); method1(c); }
+ * 
+ * 
+ * 
+ */
